dffd578148fe5d1dd4edd952dea43c66a4514517
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-10-04 21:34:49 -0500

    Add project files.

---
+++ VehicleTrackerUnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace VehicleTrackerUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ VehicleUnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace VehicleUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
